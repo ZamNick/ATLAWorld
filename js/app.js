1555d32db28c181f1f26be628c6d3aff801ef17c
Nikolay Zamulov <kolia8@mail.ru>
2017-01-03 15:53:26 +0300

    Cross browsing.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+Object.values = function(obj) {
+	return Object.keys(obj).map(function(key) {
+		return obj[key];
+	});
+}
+
 window.onload = function() {
 	Loader._instance.loadImages(function() {
 		var world = new World();
--- js/mouseController.js
+++ js/mouseController.js
@@ -4,9 +4,13 @@ function MouseController(camera) {
 
 	var _moveable = false;
 	var _self = this;
+    var _x = null;
+    var _y = null;
 
     document.addEventListener('mousedown', function(e) {
     	_moveable = true;
+        _x = e.x;
+        _y = e.y;
     });
 
     document.addEventListener('mouseup', function(e) {
@@ -16,8 +20,14 @@ function MouseController(camera) {
     document.addEventListener('mousemove', function(e) {
     	if(_moveable) {
 
+            e.movementX = e.movementX || (e.x - _x);
+            e.movementY = e.movementY || (e.y - _y);
+
     		camera.position.x -= e.movementX * 0.1;
     		camera.position.y += e.movementY * 0.1;
+
+            _x = e.x;
+            _y = e.y;
 
     		_self.updateCameraPosition();
     	}
