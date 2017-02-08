2566f7ce80631e3f98eeb1b4a0d9b85dffa77a85
Nikolay Zamulov <kolia8@mail.ru>
2017-02-08 14:03:45 +0200

    Refactoring. Change camera moving logic.

--- js/abstractController.js
+++ js/abstractController.js
@@ -3,13 +3,18 @@
 function AbstractController(camera) {
 	
 	this.updateCameraPosition = function() {
-		var scale = 1 - (camera.position.z / CONSTANTS.SCALE.MAX);
 
-		camera.position.x = Math.max(camera.position.x, -(CONSTANTS.MAP.SETTINGS.SIZE.X / 2) * scale);
-		camera.position.y = Math.max(camera.position.y, -(CONSTANTS.MAP.SETTINGS.SIZE.Y / 2) * scale);
+		var _scale = 1 - (camera.position.z / CONSTANTS.SCALE.MAX);
+		
+		var _pixelCoeff = CONSTANTS.MAP.SETTINGS.SIZE.Y / window.innerHeight;
+		var _currentImageWidthByX = _pixelCoeff * window.innerWidth;
+		var _allowByX = ((CONSTANTS.MAP.SETTINGS.SIZE.X - _currentImageWidthByX) / 2) * (1 - _scale);
 
-		camera.position.x = Math.min(camera.position.x, (CONSTANTS.MAP.SETTINGS.SIZE.X / 2) * scale);
-		camera.position.y = Math.min(camera.position.y, (CONSTANTS.MAP.SETTINGS.SIZE.Y / 2) * scale);
+		camera.position.x = Math.max(camera.position.x, -(CONSTANTS.MAP.SETTINGS.SIZE.X / 2) * _scale - _allowByX);
+		camera.position.y = Math.max(camera.position.y, -(CONSTANTS.MAP.SETTINGS.SIZE.Y / 2) * _scale);
+
+		camera.position.x = Math.min(camera.position.x, (CONSTANTS.MAP.SETTINGS.SIZE.X / 2) * _scale + _allowByX);
+		camera.position.y = Math.min(camera.position.y, (CONSTANTS.MAP.SETTINGS.SIZE.Y / 2) * _scale);
     }
 
 }
